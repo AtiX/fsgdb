4f02f4e3f8bcbdcf81c3ddc59aee7fc672a0ea2b
Arthur Silber <arthur@arthursilber.de>
2015-12-31 18:06:54 +0100

    Exports both the database and the query classes

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 // Require coffeescript and return the main module of fsdb
-require('coffee-script').register()
-module.exports = require('./src/FileSystemGraphDatabase');
+require('coffee-script').register();
+
+module.exports = {
+  FileSystemGraphDatabase: require('./src/FileSystemGraphDatabase'),
+  Query: require('./src/Query')
+};
